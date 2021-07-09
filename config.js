3042d41bf7faad6725aa5354e421158b052e4266
Catalin Ta <skillzlro@gmail.com>
2021-07-09 16:51:28 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,65 +1,66 @@
  export default {
-	about: {
-		image:
-			"https://cdn.discordapp.com/avatars/565960314970177556/a2c58e3aefa7fd6b705a3e92d7665500.png?size=1024",
-		name: "skillzl",
-		job: "self-thought programmer",
-		description: "life's short death's long so whatever",
-		socialLinks: [
+		about: {
+			image:
+				"https://cdn.discordapp.com/avatars/565960314970177556/a2c58e3aefa7fd6b705a3e92d7665500.png?size=1024",
+			name: "skillzl",
+			job: "self-thought programmer",
+			description:
+				"pay attention to the start cause you can always see the end",
+			socialLinks: [
+				{
+					name: "Github",
+					icon: "fab fa-github",
+					color: "gray",
+					to: "https://github.com/skillzl",
+				},
+				{
+					name: "Discord",
+					color: "indigo",
+					icon: "fab fa-discord",
+					to: "https://discord.com/users/565960314970177556/",
+				},
+				{
+					name: "Spotify",
+					color: "green",
+					icon: "fab fa-spotify",
+					to: "https://open.spotify.com/user/gfq8gapgnuydzkz0n34zbkcc7",
+				},
+				{
+					name: "Instagram",
+					color: "purple",
+					icon: "fab fa-instagram",
+					to: "https://www.instagram.com/underground.catalin/",
+				},
+			],
+		},
+		projects: [
 			{
-				name: "Github",
-				icon: "fab fa-github",
-				color: "gray",
-				to: "https://github.com/skillzl",
+				name: "discord-economy",
+				description:
+					"Open source Discord bot project based on economy features using quickdb as database.",
+				status: 3,
+				to: "https://github.com/skillzl/discord-economy",
 			},
 			{
-				name: "Discord",
-				color: "indigo",
-				icon: "fab fa-discord",
-				to: "https://discord.com/users/565960314970177556/",
+				name: "charliewave-me",
+				description:
+					"A multipurpose discord client written in javascript. Used to be a freelance start-up project but now it's a public client for all around the world users.",
+				status: 3,
+				to: "https://github.com/charliewave-me",
 			},
 			{
-				name: "Spotify",
-				color: "green",
-				icon: "fab fa-spotify",
-				to: "https://open.spotify.com/user/gfq8gapgnuydzkz0n34zbkcc7",
+				name: "b1gcheats",
+				description:
+					"Freelance project based on a YouTube community with a free range of resources and designs for popular market games.",
+				status: 3,
+				to: "https://github.com/b1gcheats",
 			},
 			{
-				name: "Instagram",
-				color: "purple",
-				icon: "fab fa-instagram",
-				to: "https://www.instagram.com/underground.catalin/",
+				name: "todo/application",
+				description:
+					"Web application using localStorage to save a to-do list for you.",
+				status: 3,
+				to: "https://todo.skillzl.me",
 			},
 		],
-	},
-	projects: [
-		{
-			name: "discord-economy",
-			description:
-				"Open source Discord bot project based on economy features using quickdb as database.",
-			status: 3,
-			to: "https://github.com/skillzl/discord-economy",
-		},
-		{
-			name: "charliewave-me",
-			description:
-				"A multipurpose discord client written in javascript. Used to be a freelance start-up project but now it's a public client for all around the world users.",
-			status: 3,
-			to: "https://github.com/charliewave-me",
-		},
-		{
-			name: "b1gcheats",
-			description:
-				"Freelance project based on a YouTube community with a free range of resources and designs for popular market games.",
-			status: 3,
-			to: "https://github.com/b1gcheats",
-		},
-		{
-			name: "todo/application",
-			description:
-				"Web application using localStorage to save a to-do list for you.",
-			status: 3,
-			to: "https://todo.skillzl.me",
-		},
-	],
-};
+ };
