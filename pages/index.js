d25e29daf994e638c422b99d0f7956150c13ff7d
skillzl <user@skillzl.me>
2021-12-25 21:37:21 +0200

    holiday update

--- pages/index.js
+++ pages/index.js
@@ -1,34 +1,37 @@
-import React from 'react';
+/** @format */
 
-import Portfolio from '../components/Portfolio';
-import PortfolioImage from '../components/PortfolioImage';
+import Portfolio from "../components/Portfolio";
+import PortfolioImage from "../components/PortfolioImage";
 import SocialMedia from "../components/SocialMedia";
 import Projects from "../components/Projects";
-import Layout from '../components/Layout';
+import Layout from "../components/Layout";
 
-import config from '../config';
+import config from "../config";
+
+import Snowfall from "react-snowfall";
 
 function Home() {
   return (
-		<Layout>
-			<div className='container max-w-screen-lg mx-auto'>
-				<div>
-					<div className='flex flex-wrap items-start justify-start w-full'>
-						<PortfolioImage image={config.about.image} />
-						<SocialMedia socialLinks={config.about.socialLinks} />
-						<Portfolio
-							name={config.about.name}
-							job={config.about.job}
-							description={config.about.description}
-						/>
-					</div>
-				</div>
-				<div className='flex flex-wrap w-full space-y-2 md:space-x-4 md:flex-no-wrap'>
-					<Projects projects={config.projects} />
-				</div>
-			</div>
-		</Layout>
-	);
+    <Layout>
+      <div className="container max-w-screen-lg mx-auto">
+        <Snowfall />
+        <div>
+          <div className="flex flex-wrap items-start justify-start w-full">
+            <PortfolioImage image={config.about.image} />
+            <SocialMedia socialLinks={config.about.socialLinks} />
+            <Portfolio
+              name={config.about.name}
+              job={config.about.job}
+              description={config.about.description}
+            />
+          </div>
+        </div>
+        <div className="flex flex-wrap w-full space-y-2 md:space-x-4 md:flex-no-wrap">
+          <Projects projects={config.projects} />
+        </div>
+      </div>
+    </Layout>
+  );
 }
 
 export default Home;
