6e0a2f3a84770d853b03c48358b0f333219215fe
skillzl <user@skillzl.me>
2021-12-26 17:23:02 +0200

    meta img

--- pages/_app.js
+++ pages/_app.js
@@ -16,11 +16,11 @@ function App({ Component, pageProps }) {
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <title>skillzl</title>
-        <meta name="description" content="Personal portfolio." />
+        <meta name="description" content="18 yo, self-thought programmer" />
         <meta name="author" content="skillzl/index" />
         <meta name="robots" content="index, follow" />
         <meta name="og:theme-color" content="#0f2233" />
-        <meta property="og:image" content="https://i.imgur.com/jcQSGil.png" />
+        <meta property="og:image" content="https://i.imgur.com/zJFoAWO.png" />
 
         <link
           href="https://unpkg.com/tailwindcss@^1.0/dist/tailwind.min.css"
