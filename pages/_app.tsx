f3a8e70557d766f471e42a7a1e0d5a017de414e4
skillzl <user@skillzl.me>
2022-06-23 15:23:49 +0300

    :bug: bug: console error flags

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,18 +1,15 @@
 import { ThemeProvider } from 'next-themes'
-import { SessionProvider } from 'next-auth/react'
 import type { AppProps } from 'next/app'
 import AppLayout from 'layouts/AppLayout'
 import 'styles/globals.css'
 import 'styles/prism.css'
 
-function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
+function MyApp({ Component, pageProps: { ...pageProps } }: AppProps) {
   return (
     <ThemeProvider attribute="class" defaultTheme="system" disableTransitionOnChange>
-      <SessionProvider session={session}>
         <AppLayout>
           <Component {...pageProps} />
         </AppLayout>
-      </SessionProvider>
     </ThemeProvider>
   )
 }
