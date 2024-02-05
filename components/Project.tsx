8ea0df55022291c23493448bf2d06c1407335614
Catalin Ta <admin@skillzl.dev>
2024-02-05 23:21:35 +0200

    📂 content: project

--- components/Project.tsx
+++ components/Project.tsx
@@ -22,7 +22,7 @@ export default function Project({ title, author, year, comment, active, over, li
       className="font-body max-w-md list-none overflow-hidden rounded-xl bg-white shadow dark:border-gray-700
       dark:bg-gradient-to-tr from-gray-900 to-zinc-900 sm:w-full sm:max-w-4xl"
     >
-      <article className="px-6 pt-6 sm:flex sm:pb-6 duration-300 hover:p-7">
+      <article className="px-6 pt-6 sm:flex sm:pb-6">
         <div className="w-full py-6 sm:py-0 sm:px-6">
           <h2 className="mb-1.5 space-x-2 text-xl font-semibold text-gray-900 dark:text-white">
             {title}
@@ -37,9 +37,9 @@ export default function Project({ title, author, year, comment, active, over, li
           </div>
           <p className="py-1 leading-relaxed text-gray-600 dark:text-gray-400">{`${comment}`}</p>
           <div className="flex space-x-2 flex-row">
-          <a href={link}>
+          {link && <a href={link}>
             <Github className="h-4 w-4 fill-gray-400 duration-300 hover:fill-black hover:dark:fill-white hover:h-5 hover:w-4" />
-          </a>
+          </a>}
             <br />
             {website && <a href={weblink}>
             <GlobeAltIcon className="h-4 w-4 fill-gray-400 duration-300 hover:fill-black hover:dark:fill-white hover:h-5 hover:w-4" />
